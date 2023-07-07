043bd41efb8fc94685c2a7fb2d1e07b3cb435bdd
Toby <5722599@gmail.com>
2023-07-07 19:31:33 -0400

    added header

--- practice-01/src/GameList.js
+++ practice-01/src/GameList.js
@@ -3,13 +3,16 @@ import { games } from "./data";
 
 const GameList = () => {
   return (
-    <section className="gamelist">
-      {games.map((game) => {
-        return (
-          <Game game={game} key={game.id} />
-        )
-      })}
-    </section>
+    <>
+      <h1 className="header">amazon best sellers</h1>
+      <section className="gamelist">
+        {games.map((game) => {
+          return (
+            <Game game={game} key={game.id} />
+          )
+        })}
+      </section>
+    </>
   )
 }
 export default GameList;
