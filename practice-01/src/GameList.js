f76f9e2b38c494cb6777f49936ea741bc1271079
Toby <5722599@gmail.com>
2023-07-07 19:03:17 -0400

    separated game data into data.js file

--- practice-01/src/GameList.js
+++ practice-01/src/GameList.js
@@ -1,37 +1,14 @@
 import { Game } from "./Game";
-
-import game01 from './img/Game01.jpg'
-import game02 from './img/Game02.jpg'
-import game03 from './img/Game03.jpg'
-import game04 from './img/Game04.jpg'
+import { games } from "./data";
 
 const GameList = () => {
   return (
     <section className="gamelist">
-      <Game game={{
-        title: 'Mario Kart 8 Deluxe',
-        platform: 'Nintendo Switch',
-        img: game01,
-        price: '$65.00',
-      }} />
-      <Game game={{
-        title: 'The Legend of Zelda™: Tears of the Kingdom',
-        platform: 'Nintendo Switch',
-        img: game02,
-        price: '$89.47',
-      }} />
-      <Game game={{
-        title: 'Mario Party Superstars',
-        platform: 'Nintendo Switch',
-        img: game03,
-        price: '$68.94',
-      }} />
-      <Game game={{
-        title: 'Hogwarts Legacy',
-        platform: 'PlayStation 4',
-        img: game04,
-        price: '$69.96',
-      }} />
+      {games.map((game) => {
+        return (
+          <Game game={game} key={game.id} />
+        )
+      })}
     </section>
   )
 }
---
+++ practice-01/src/data.js
@@ -0,0 +1,36 @@
+
+import game01 from './img/Game01.jpg'
+import game02 from './img/Game02.jpg'
+import game03 from './img/Game03.jpg'
+import game04 from './img/Game04.jpg'
+
+export const games = [
+  {
+    title: 'Mario Kart 8 Deluxe',
+    platform: 'Nintendo Switch',
+    img: game01,
+    price: '$65.00',
+    id: 0,
+  },
+  {
+    title: 'The Legend of Zelda™: Tears of the Kingdom',
+    platform: 'Nintendo Switch',
+    img: game02,
+    price: '$89.47',
+    id: 1,
+  },
+  {
+    title: 'Mario Party Superstars',
+    platform: 'Nintendo Switch',
+    img: game03,
+    price: '$68.94',
+    id: 2,
+  },
+  {
+    title: 'Hogwarts Legacy',
+    platform: 'PlayStation 4',
+    img: game04,
+    price: '$69.96',
+    id: 3,
+  }
+]
