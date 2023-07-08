cbbef9525fbf9ac94eb928fa6d439632a09e50be
Toby <5722599@gmail.com>
2023-07-08 10:39:31 -0400

    practice-01 all done

--- practice-01/src/Game.js
+++ practice-01/src/Game.js
@@ -1,11 +1,15 @@
 export const Game = (props) => {
-  const { img, title, platform, price } = props.game;
+  const { img, title, platform, price, id } = props.game;
   return (
     <div className='game'>
       <img src={img} alt={title} />
       <h1>{title}</h1>
       <p>{platform}</p>
       <p className="price">{price}</p>
+      <div className="rank">
+        <p>#{id + 1}</p>
+      </div>
+      <div className="triangle"></div>
     </div>
   )
 }
