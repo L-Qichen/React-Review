58e5a6172da755a9f4a7f2ea8bf52ff3dcf53f17
Toby <5722599@gmail.com>
2023-07-12 17:01:03 -0400

    Added README file to this repo; added some comment into practice-00

---
+++ README.md
@@ -0,0 +1,3 @@
+The purpose of this repo is a systematically review the fundamentals react and learn some new features of react18:
+
+1. practice-00 - how to create and render component.
--- practice-00/src/index.js
+++ practice-00/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// create component
+// usually separate component in different files, and 
+// import it when we need to use it
 const Greeting = () => {
   return <h1>Hello, this is a simple react component.</h1>
 }
 
+// create root dom
+// root is like the entry of react
+// it links to index.html and include everything of react stuff
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// render component on page
 root.render(<Greeting />);
