f1eeff58b3ebaa904612748244e70c3be6b1f216
Toby <5722599@gmail.com>
2023-07-15 20:14:12 -0400

    finished FirstExample to show the data cannot be dynamic renew on the page

--- practice-02/src/FirstExample.jsx
+++ practice-02/src/FirstExample.jsx
@@ -1,4 +1,16 @@
 import './FirstExample.css'
+let count = 0;
 export const FirstExample = () => {
-  return <h3 className='example'>This example shows why we need UseState hook:</h3>
+  return (
+    <>
+      <h3 className='example'>This example shows why we need UseState hook:</h3>
+      <div className='main'>
+        <h3>{count}</h3>
+        <button type='button' onClick={() => {
+          count += 1;
+          console.log({ count });
+        }}>increase</button>
+      </div>
+    </>
+  )
 }
