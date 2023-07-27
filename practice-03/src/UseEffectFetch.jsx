634b9e731663b184decb16a8fd3c58ee5a5ab8eb
Toby <5722599@gmail.com>
2023-07-27 15:17:43 -0400

    demonstrate the user list on page

--- practice-03/src/UseEffectFetch.jsx
+++ practice-03/src/UseEffectFetch.jsx
@@ -4,17 +4,28 @@ import './UseEffect.css'
 const url = 'https://api.github.com/users';
 
 export const UseEffectFetch = () => {
-  const [userList, setuserList] = useState([]);
+  const [userList, setUserList] = useState([]);
 
   useEffect(() => {
-    fetch(url).then(response => response.json()).then(console.log);
+    fetch(url).then(response => response.json()).then(setUserList);
   }, [])
 
   return (
     <>
       <h1 className="title" style={{ color: "#D781E3" }}>Use Effect Basic</h1>
       <div className="main" style={{ color: "#D781E3" }}>
-        {userList}
+        <ol>
+          {userList.map((user) => {
+            const { id, login, avatar_url, html_url } = user;
+            return <li key={id}>
+              <img src={avatar_url} />
+              <div>
+                <h5>{login}</h5>
+                <a href="html_url"> Profile </a>
+              </div>
+            </li>
+          })}
+        </ol>
       </div>
     </>
   )
