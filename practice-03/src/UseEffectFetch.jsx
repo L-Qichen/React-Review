b4e711a4374caddff3c88182860f04556b5a04ad
Toby <5722599@gmail.com>
2023-07-27 15:48:55 -0400

    UseEffectFetch component done. Added styles for it

--- practice-03/src/UseEffectFetch.jsx
+++ practice-03/src/UseEffectFetch.jsx
@@ -18,10 +18,10 @@ export const UseEffectFetch = () => {
           {userList.map((user) => {
             const { id, login, avatar_url, html_url } = user;
             return <li key={id}>
-              <img src={avatar_url} />
+              <img src={avatar_url} alt={login} />
               <div>
                 <h5>{login}</h5>
-                <a href="html_url"> Profile </a>
+                <a href={html_url}> Profile </a>
               </div>
             </li>
           })}
