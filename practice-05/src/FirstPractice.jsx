86104fde161c542717ac7178cbbe8e0f2535cf73
Toby <5722599@gmail.com>
2023-08-08 17:21:33 -0400

    FirstPractice component done

--- practice-05/src/FirstPractice.jsx
+++ practice-05/src/FirstPractice.jsx
@@ -4,6 +4,31 @@ import { customers } from './data'
 
 export const FirstPractice = () => {
   const [name, setName] = useState('');
+  const [customersList, setCustomersList] = useState(customers);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) return;
+    const fakeId = Date.now();
+    const newCustomer = {
+      id: fakeId,
+      name
+    }
+    setCustomersList([...customersList, newCustomer]);
+    setName('');
+  }
+  const handleRemove = (id) => {
+    // Important:
+    // when we use the arrow function, be careful to
+    // use return in {}(function body)
+    // if we don't use return, it means this function no
+    // return value.
+    // When there only is one statement or value in {}
+    //(function body), we can omit {} and return.
+    // in this case: 
+    // (customer) => { return customer.id !== id } === customer => customer.id !== id
+    const newCustomersList = customersList.filter((customer) => { return customer.id !== id });
+    setCustomersList(newCustomersList);
+  }
 
   return (
     <>
@@ -12,15 +37,26 @@ export const FirstPractice = () => {
         <form>
           <div className='form-row'>
             <label htmlFor='customerName' className='form-label'>Name:</label>
-            <input id='customerName' type='text' className='form-input'></input>
-            <button type='submit' className='btn'>Add</button>
+            <input id='customerName' type='text' className='form-input'
+              value={name} onChange={e => { setName(e.target.value) }}></input>
+            <button type='submit' className='btn' onClick={handleSubmit}>Add</button>
           </div>
         </form>
         <hr />
         <ol>
-          {customers.map((customer) => {
+          {customersList.map((customer) => {
             return (
-              <li key={customer.id}>{customer.name}</li>
+              <li key={customer.id}>
+                {customer.name}
+                {/* important:
+                here we need to use callback function in 
+                OnClick event.
+                So, we cannot use onClick(handleRemove(customer.id)) here.
+                Since it is not a callback and the handleRemove
+                function will be called immediately.
+                */}
+                <button type='button' onClick={() => { handleRemove(customer.id) }}>remove</button>
+              </li>
             )
           })}
         </ol>
