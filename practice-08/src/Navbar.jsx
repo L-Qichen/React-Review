7d8816bafcc7ab02d9cf02d19a06ca3240574fa1
Toby <5722599@gmail.com>
2023-08-09 17:30:26 -0400

    navbar style done

--- practice-08/src/Navbar.jsx
+++ practice-08/src/Navbar.jsx
@@ -1,10 +1,11 @@
 import NavLinks from "./NavLinks";
+import './style.css'
 
 const Navbar = () => {
 
   return (
     <nav className='navbar'>
-      <h4 className='title'>Context API</h4>
+      <h4>Context API</h4>
       <NavLinks />
     </nav>
   )
