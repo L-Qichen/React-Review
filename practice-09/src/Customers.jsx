e9859cd8ee80bf485b66fe34b872adc87a46d5a8
Toby <5722599@gmail.com>
2023-08-19 14:49:11 -0400

    Added reset button and onClick event

--- practice-09/src/Customers.jsx
+++ practice-09/src/Customers.jsx
@@ -5,16 +5,28 @@ import './style.css'
 
 const Customers = () => {
   const [customerList, setCustomerList] = useState(customers);
+
   const removeCustomer = (id) => {
     setCustomerList(customerList.filter((customer) => {
       return customer.id !== id;
     }));
   }
 
+  const resetList = () => {
+    setCustomerList(customers);
+  };
+
+  const clearList = () => {
+    setCustomerList([]);
+  };
+
+  // show the reset button and onclick event to reset the customer 
+  // list once the list is empty.
+
   return (
     <div style={{ color: "#D2686A" }}>
       <div className='main'>
-        <h3 className='title' style={{ marginBottom: "2rem" }}>The first practice</h3>
+        <h3 className='title' style={{ marginBottom: "2rem" }}>The first practice(Add reset function)</h3>
         <ol>
           {customerList.map((customer) => {
             const { id, name } = customer;
@@ -23,9 +35,13 @@ const Customers = () => {
               <button type="button" onClick={() => removeCustomer(id)}>Remove</button>
             </li>);
           })}
-          <button type="button" className="btn" onClick={() => {
-            setCustomerList([]);
-          }}>Delete All</button>
+          {customerList.length <= 0 ?
+
+            (
+              <button type="button" className="btn" onClick={resetList}>Reset</button>
+            ) : (
+              <button type="button" className="btn" onClick={clearList}>Delete All</button>
+            )}
         </ol>
       </div>
     </div >
